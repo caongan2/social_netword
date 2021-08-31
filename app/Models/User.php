ad8535f8b138f863c2b27088b3bb0b9772140d25
cuongpham270695 <cuong.pham270695@gmail.com>
2021-08-31 22:02:38 +0700

    cuong-create-table

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,10 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'phone',
+        'birth_date',
+        'address',
+        'interest'
     ];
 
     /**
