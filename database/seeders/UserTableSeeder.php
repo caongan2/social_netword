d4b32891fb9e8207a03b21c38300d236beb1ef36
cuongpham270695 <cuong.pham270695@gmail.com>
2021-08-31 22:31:58 +0700

    update

--- database/seeders/UserTableSeeder.php
+++ database/seeders/UserTableSeeder.php
@@ -15,12 +15,13 @@ class UserTableSeeder extends Seeder
     public function run()
     {
         $user = new User();
-        $user->name = 'An';
+        $user->name = "An";
         $user->email = 'an@gmail.com';
         $user->password = '123456';
         $user->phone = '0984785142';
-        $user->birth_date = 1999-05-28;
+        $user->birth_date = 1999-05-29;
         $user->address = 'HN';
         $user->interest = 'Thich da bong';
+        $user->save();
     }
 }
