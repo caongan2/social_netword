33f103690202a77c468e4f7a1cff3ad09cf0d1e0
Ninh02 <ninhdo10021998@gmail.com>
2021-09-01 15:21:49 +0700

    CRUD_post

--- routes/api.php
+++ routes/api.php
@@ -14,8 +14,13 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+Route::prefix('posts')->group(function (){
+    Route::get('', [PostController::class, 'index']);
+    Route::post('/create', [PostController::class, 'create']);
+    Route::post('/{id}/update', [PostController::class, 'update']);
+    Route::post('/{id}/delete', [PostController::class, 'delete']);
+});
 
-Route::get('posts', [PostController::class, 'index']);
 
 
 
