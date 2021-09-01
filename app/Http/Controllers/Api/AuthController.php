b316c7a5338e6cc9fb159df6aa029ec02d27db15
cuongpham270695 <cuong.pham270695@gmail.com>
2021-09-01 15:46:11 +0700

    a

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -4,17 +4,47 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Validator;
+
 
 class AuthController extends Controller
 {
-
     public function __construct()
     {
-
+        $this->middleware('auth:api', ['except' => [ 'register']]);
     }
 
-    public function userProfile()
+
+    public function register(Request $request)
     {
-        return response()->json(auth()->user());
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|between:2,100',
+            'email' => 'required|email|max:100|unique:users',
+            'password' => 'required|confirmed|min:6|max:8',
+            'phone' => 'required|regex:/^(0+[0-9]{9})$/|unique:users',
+        ]);
+
+        if ($validator->fails()) {
+//            return response()->json($validator->errors(), 400);
+            return response()->json([
+                $validator->errors()->toJson(),
+                'message' => 'Email đã tồn tại!',
+                'error' => 'email'
+            ], 400);
+        }
+
+        $user = User::create(array_merge(
+            $validator->validated(),
+            [
+                'password' => bcrypt($request->password),
+            ]
+        ));
+
+        return response()->json([
+            'message' => 'Bạn đã đăng ký thành công!',
+            'user' => $user
+        ], 201);
     }
+
 }
