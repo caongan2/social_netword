fe5f94e9a47d8099c3f9a592e59ed5b6a174e5a9
hoaithuong2002 <dgth2o2@gmail.com>
2021-09-06 08:51:58 +0700

    update

--- routes/api.php
+++ routes/api.php
@@ -35,7 +35,7 @@ Route::group(['middleware' => 'api'], function () {
         Route::post('/logout', [AuthController::class, 'logout']);
         Route::post('/change-password', [AuthController::class, 'changePassword'])->name('change.password');
 
-        // Route::get('/user-profile', [AuthController::class, 'userProfile']);
+        Route::get('/user-profile', [AuthController::class, 'userProfile']);
         Route::prefix('users')->group(function (){
             Route::put('{id}/update-profile',[UserController::class,'update']);
         });
